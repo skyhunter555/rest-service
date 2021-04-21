d65500e0b36a7c28e7570226c6f96dde534539ad
Савоськин Дмитрий Петрович (4043747) <dpsavoskin@vtb.ru>
2021-04-21 17:37:55 +0300

    fix checking

--- final/api-gateway/src/main/java/ru/diasoft/micro/GatewayApplication.java
+++ final/api-gateway/src/main/java/ru/diasoft/micro/GatewayApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 /**
  * GatewayApplication
  *
@@ -19,6 +21,8 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 public class GatewayApplication {
 
+    Integer serviceDemoPort = 7081;
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
@@ -26,14 +30,19 @@ public class GatewayApplication {
     @Bean
     public RouteLocator createRoutes(RouteLocatorBuilder builder, DiscoveryClient discoveryClient) {
 
-        ServiceInstance serviceInstanceFirst = discoveryClient.getInstances("skyhunter-demo-service").get(0);
+        List<ServiceInstance> serviceInstance = discoveryClient.getInstances("skyhunter-demo-service");
+        if (serviceInstance != null && serviceInstance.size() > 0) {
+            ServiceInstance serviceDemoInstance = discoveryClient.getInstances("skyhunter-demo-service").get(0);
+            serviceDemoPort = serviceDemoInstance.getPort();
+        }
 
         return builder.routes()
-                .route(p -> p
-                        .path("/v1/sms-verification/**")
-                        .uri("http://" + serviceInstanceFirst.getHost() + ":" + serviceInstanceFirst.getPort() + "/")
-                        .id("skyhunter-demo")
-                )
-                .build();
+                    .route(p -> p
+                            .path("/v1/sms-verification/**")
+                            .uri("http://localhost:" + serviceDemoPort + "/")
+                            .id("skyhunter-demo")
+                    )
+                    .build();
+
     }
 }
